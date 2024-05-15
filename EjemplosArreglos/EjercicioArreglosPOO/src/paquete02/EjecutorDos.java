bd7616808e5218ecac87445ceb9ebf5fffe6f1ef
geloyola <gabyloyola1995@gmail.com>
2024-05-15 16:01:10 -0500

    ejecutor2

---
+++ EjemplosArreglos/EjercicioArreglosPOO/src/paquete02/EjecutorDos.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paquete02;
+import paquete01.LibretaCalificacion;
+/**
+ *
+ * @author utpl
+ */
+public class EjecutorDos {
+    public static void main(String[]args){
+        double[] notas = {10,9.0,10};
+        String nombreEstudiante = "Rene Elizalde";
+        
+         LibretaCalificacion libreta = new LibretaCalificacion(nombreEstudiante,
+               notas);
+        
+        libreta.establecerPromedio();
+        libreta.establecerPromedioCualitativo();
+        System.out.printf("\n%s\n", libreta);
+    
+}
+}
